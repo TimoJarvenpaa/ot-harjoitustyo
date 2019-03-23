f93d5aba2047f365bdd6e615061b835c07a080a2
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-03-23 13:15:24 +0200

    Viikon 2 laskarit, tehtävä 2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals(20, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertEquals(5, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosTarpeeksiRahaa() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosEiTarpeeksiRahaa() {
+        assertFalse(kortti.otaRahaa(15));
+    }
 }
